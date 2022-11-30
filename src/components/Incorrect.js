d33a92aa693f61a513d6cd8100ae4f281c6ac7ef
Chris Tomshack <ctomshack15@gmail.com>
2022-11-30 17:51:58 -0500

    finished MVP version

--- src/components/Incorrect.js
+++ src/components/Incorrect.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 export default function Incorrect({ incorrect }) {
   return (
-    <div>
+    <div className='grid grid-cols-1 gap-8 md:px-12'>
+      <p className='text-left underline text-white text-xl font-bold md:mt-4'>Incorrectly answered questions</p>
       {incorrect.map((elem, idx) => {
         return (
-          <div key={idx} className="flex-col">
-            <h3 className="text-xl text-red-500 font-bold my-0 drop-shadow-md">
+          <div key={idx} className="grid grid-rows-2 md:grid-rows-1 md:grid-cols-2 border-b">
+            <h3 className="row-start-1 row-span-1 md:col-start-1 md:col-span-1 text-xl text-left text-orange-500 font-base my-0">
               {elem.question}
             </h3>
-            <h3 className="text-xl text-white font-bold mb-8 drop-shadow-md">
-              Answer: {elem.correctAnswer}
-            </h3>
+            <div className='flex flex-col-reverse'>
+            <p className="row-start-2 row-span-1 md:col-start-2 md:col-span-2 text-xl text-left md:text-right text-white font-base font-bold ">
+               {elem.correctAnswer}
+            </p>
+            </div>
           </div>
         );
       })}
