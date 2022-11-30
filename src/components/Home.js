9fe16da63b0c89bec118d8811f91bbb2f2699e98
Chris Tomshack <ctomshack15@gmail.com>
2022-11-30 18:01:40 -0500

    updated button background colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
                     <Link to='/SelectCategory'
                     className='w-full'>
                         <button
-                        className="my-2 text-5xl font-bold hover:shadow-md block text-center w-full lg:w-full justify-center items-center  rounded-md border border-gray-300 bg-white px-6 py-8 text-gray-700 shadow-sm hover:bg-orange-500 hover:text-white  hover:border-orange-500 border-r-2 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2"
+                        className="my-2 text-5xl font-bold hover:shadow-md block text-center w-full lg:w-full justify-center items-center  rounded-md  bg-gray-700 px-6 py-8 text-white opacity-80 shadow-sm hover:bg-orange-500 hover:text-white  hover:border-orange-500 focus:outline-none focus:ring-2 focus:ring-white"
                         >Play Now</button>
                     </Link>
         </div>
