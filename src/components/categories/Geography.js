3b699533c78754d5b21b40c0125345f706ddd587
Chris Tomshack <ctomshack15@gmail.com>
2022-12-02 19:14:01 -0500

    updated question box styling

--- src/components/categories/Geography.js
+++ src/components/categories/Geography.js
@@ -77,11 +77,11 @@ export default function Geography() {
     <>
         {!isDone ? (
           <div className="center-screen flex-col justify-center align-middle">
-            <h1 className="text-6xl md:text-8xl text-white font-extrabold mb-8 drop-shadow-xl">
+            <h1 className="text-4xl mt-8 lg:mt-4 lg:mt-auto md:text-6xl text-white font-extrabold mb-4 md:mb-8 drop-shadow-xl">
               {questionObject.category}
             </h1>
-            <div className="grid lg:grid-cols-2 gap-4 lg:gap-8 bg-gray-700 py-16 px-8 mx-8 lg:mx-28 my-6 rounded-md shadow-lg opacity-80 min-w-100 max-w-500">
-              <div className="mb-8">
+            <div className="grid lg:grid-cols-2 gap-4 lg:gap-8 bg-gray-700 py-16 px-8 mx-8 lg:mx-28 my-6 md:min-h-608  lg:h-392 rounded-md shadow-lg opacity-80 w-4/5">
+              <div className="mb-8 h-48 lg:h-48 lg:w-96">
                 <h2 className="text-4xl text-left mb-4 text-white font-bold">
                   Question {index + 1}/{questions.length}
                 </h2>
